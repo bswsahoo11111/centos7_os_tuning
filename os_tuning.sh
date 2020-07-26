21af0e95eee9b1953331772e0d39876e2fdd7ba3
root <root@dn1.us-central1-a.c.unique-alpha-284503.internal>
2020-07-26 10:30:15 +0000

    first commit

---
+++ os_tuning.sh
@@ -0,0 +1,13 @@
+sudo -s
+echo "team@123" | passwd --stdin root
+sed -i s/'PermitRootLogin no'/'PermitRootLogin yes'/g /etc/ssh/sshd_config;
+sed -i s/'PasswordAuthentication no'/'PasswordAuthentication yes'/g /etc/ssh/sshd_config;
+sed -i s/'ChallengeResponseAuthentication no'/'ChallengeResponseAuthentication yes'/g /etc/ssh/sshd_config;
+systemctl restart sshd
+sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
+setenforce 0
+getenforce
+systemctl disable firewalld; systemctl stop firewalld; systemctl status firewalld
+yum install wget git ansible –y
+yum update -y
+
